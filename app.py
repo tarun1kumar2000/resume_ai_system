ea59423475d9c21c8615864f56e7ec275fdb645c
tarun1kumar2000 <tarun1kumar2006@gmail.com>
2026-08-06 00:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 from parser_utils import extract_text_from_pdf, extract_text_from_docx
-from google import genai
+from ai_engine import get_analysis_from_ai
 from styles import apply_custom_css
 
 # Page Config
