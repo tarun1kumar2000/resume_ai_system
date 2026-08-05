65e07d972c7126a75cefd428dc9f3defe37097a9
tarun1kumar2000 <tarun1kumar2006@gmail.com>
2026-08-06 01:29:55 +0530

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -1,47 +1,33 @@
-import requests
+from openai import OpenAI
 import json
 from prompts import RESUME_ANALYSIS_PROMPT
 
-API_URL = "https://openrouter.ai/api/v1/chat/completions"
-
 
 def get_analysis_from_ai(resume_text, api_key):
+
+    client = OpenAI(
+        api_key=api_key,
+        base_url="https://openrouter.ai/api/v1"
+    )
 
     prompt = RESUME_ANALYSIS_PROMPT.format(
         resume_text=resume_text
     )
 
-    headers = {
-        "Authorization": f"Bearer {api_key}",
-        "Content-Type": "application/json",
-        "HTTP-Referer": "https://streamlit.io",
-        "X-Title": "AI Resume Screener"
-    }
-
-    payload = {
-        "model": "meta-llama/llama-3.3-70b-instruct:free",
-        "messages": [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        "temperature": 0.2
-    }
-
     try:
-        response = requests.post(
-            API_URL,
-            headers=headers,
-            json=payload,
-            timeout=120
+
+        response = client.chat.completions.create(
+            model="meta-llama/llama-3.3-70b-instruct:free",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            temperature=0.2
         )
 
-        response.raise_for_status()
-
-        result = response.json()
-
-        text = result["choices"][0]["message"]["content"]
+        text = response.choices[0].message.content
 
         text = text.replace("```json", "")
         text = text.replace("```", "")
